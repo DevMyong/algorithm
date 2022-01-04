173ba033c30988a74985dbf816f6992f55efa74c
DevMyong <myong.dev@gmail.com>
2022-01-04 23:07:48 +0900

    Add ch12 prob2

---
+++ internal/python-for-coding-test/implementation/문자열_재정렬.py
@@ -0,0 +1,15 @@
+def solution(s):
+    ss = sorted(list(s))
+
+    idx = 0
+    for i in range(len(ss)):
+        if ss[i] >= 'A':
+            idx = i
+            break
+
+    return ''.join(ss[idx:] + ss[:idx])
+
+
+if __name__ == "__main__":
+    s = input()
+    print(solution(s))
